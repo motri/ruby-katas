dfcd1d93e512d1dc9b31d43b502d89e59fcb0214
Unai Motriko <unaimotriko@gmail.com>
2017-03-30 22:12:06 +0100

    Spin words kata

---
+++ 6kyu/spin_words.rb
@@ -0,0 +1,18 @@
+=begin
+Write a function that takes in a string of one or more words,
+and returns the same string,
+but with all five or more letter words reversed
+(Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+=end
+
+def spinWords(string)
+  words = string.split(" ")
+  words.each do |x|
+      if x.length >= 5
+      x.reverse!
+      end
+  end
+      new_string = words.join(" ")
+end
