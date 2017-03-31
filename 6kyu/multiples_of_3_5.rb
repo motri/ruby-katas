dc7b0e115e2d0d1dc34d2577e22a74b754246741
Unai Motriko <unaimotriko@gmail.com>
2017-03-31 23:44:29 +0100

    Multiples of 3 and 5 kata refactored

--- 6kyu/multiples_of_3_5.rb
+++ 6kyu/multiples_of_3_5.rb
@@ -1,7 +1,14 @@
+=begin
 def solution(number)
  number_array = [0]
  for n in 1...number
  number_array << n if n % 3 == 0 || n % 5 == 0
  end
  return number_array.inject(:+)
+end
+=end
+
+#refactored
+def solution(number)
+  (1...number).select {|i| i%3==0 || i%5==0}.inject(:+)
 end
