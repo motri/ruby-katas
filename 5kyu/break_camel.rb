34b3964f361f35605855f9f44e3f30c97d8ea5f8
Unai Motriko <unaimotriko@gmail.com>
2017-03-31 23:35:58 +0100

    Break camel case kata refactored

--- 5kyu/break_camel.rb
+++ 5kyu/break_camel.rb
@@ -5,6 +5,5 @@ up camel casing, using a space between words.
 solution('camelCasing') # => should return 'camel Casing'
 =end
 def solution(string)
-  result = string.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
-  return result
+  string.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
 end
